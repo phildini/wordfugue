0aa46aeaf73abc01c6b624b7e9f6127e3dd3875b
Philip James <pjj@philipjohnjames.com>
2016-02-21 15:43:29 -0800

    Make improvements for heroku

--- wordfugue/wordfugue/prod_settings/__init__.py
+++ wordfugue/wordfugue/prod_settings/__init__.py
@@ -29,3 +29,17 @@ OPBEAT = {
     'APP_ID': get_env_variable("OPBEAT_APP_ID"),
     'SECRET_TOKEN': get_env_variable("OPBEAT_SECRET_KEY"),
 }
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
