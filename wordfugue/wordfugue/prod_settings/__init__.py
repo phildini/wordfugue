703284d8b4df012f6c8c3211da865281fdff581a
Philip James <pjj@philipjohnjames.com>
2016-02-21 16:03:51 -0800

    Stop trying to serve static from django

--- wordfugue/wordfugue/prod_settings/__init__.py
+++ wordfugue/wordfugue/prod_settings/__init__.py
@@ -30,16 +30,4 @@ OPBEAT = {
     'SECRET_TOKEN': get_env_variable("OPBEAT_SECRET_KEY"),
 }
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_URL = '//logtacts.s3.amazonaws.com/assets/'
