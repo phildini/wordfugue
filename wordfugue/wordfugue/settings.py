a5e26dac749312c55102b574708b7758d28d5ca2
Philip James <pjj@philipjohnjames.com>
2016-02-25 16:48:44 -0800

    Add flatpages

--- wordfugue/wordfugue/settings.py
+++ wordfugue/wordfugue/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.flatpages',
     'blog',
     'home',
     'suit_redactor',
--- wordfugue/wordfugue/urls.py
+++ wordfugue/wordfugue/urls.py
@@ -5,5 +5,6 @@ from home.views import HomeView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^p/', include('django.contrib.flatpages.urls')),
     url(r'^', include('blog.urls', namespace='blog')),
 ]
