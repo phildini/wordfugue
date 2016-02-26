db0be40bd1df8d7614259a258f748d356fb82bde
Philip James <pjj@philipjohnjames.com>
2016-02-26 15:18:22 -0800

    Add about page to navbar, adjust site title

--- wordfugue/blog/middleware.py
+++ wordfugue/blog/middleware.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.sites.models import Site
 
 class SetSiteMiddleware(object):
@@ -5,5 +6,6 @@ class SetSiteMiddleware(object):
     def process_request(self, request):
         try:
             request.site = Site.objects._get_site_by_request(request)
+            settings.SITE_ID = request.site.id
         except Site.DoesNotExist:
             request.site = None
