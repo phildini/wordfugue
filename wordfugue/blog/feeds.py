52ee08c2a74e2948a4cd1b9e22945a34abe96e5a
Philip James <pjj@philipjohnjames.com>
2016-02-27 19:25:38 -0800

    Add feeds

---
+++ wordfugue/blog/feeds.py
@@ -0,0 +1,36 @@
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse
+from django.http import Http404
+from .models import BlogPost
+
+
+class BlogFeed(Feed):
+
+    def get_feed(self, obj, request):
+        self.site = request.site
+        if not self.site:
+            raise Http404()
+        return super(BlogFeed, self).get_feed(obj, request)
+
+    def title(self):
+        return self.site.name
+
+    def link(self):
+        return self.site.domain
+
+    def feed_url(self):
+        return "{}/feed/".format(self.site.domain)
+
+    def description(self):
+        return "The latest posts from {}".format(self.site.name)
+
+    def items(self):
+        return BlogPost.objects.get_published_posts_for_site(self.site).order_by(
+            '-publish_date'
+        )[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.body
--- wordfugue/wordfugue/urls.py
+++ wordfugue/wordfugue/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from blog.feeds import BlogFeed
 from home.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^feed/', BlogFeed()),
     url(r'^p/', include('django.contrib.flatpages.urls')),
     url(r'^', include('blog.urls', namespace='blog')),
 ]
