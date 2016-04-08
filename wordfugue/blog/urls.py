ea4b9ced5ccea02ca0bc2135e670260610a5a050
Philip James <pjj@philipjohnjames.com>
2016-04-08 10:18:17 -0700

    Add tagged post view

--- wordfugue/blog/urls.py
+++ wordfugue/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^(?P<slug>[\w-]+)/$', views.BlogPostView.as_view(), name="post"),
     url(r'^preview/(?P<slug>[\w-]+)/$', views.BlogPostPreviewView.as_view()),
+    url(r'^tag/(?P<slug>[\w-]+)/$', views.TaggedPostView.as_view()),
 ]
--- wordfugue/blog/views.py
+++ wordfugue/blog/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from . import models
 from django.contrib.sites.shortcuts import get_current_site
+
+from django.shortcuts import (
+    get_object_or_404,
+    redirect,
+)
 
 from braces.views import (
     LoginRequiredMixin,
@@ -22,3 +27,25 @@ class BlogPostPreviewView(LoginRequiredMixin, StaffuserRequiredMixin, DetailView
 
     model = models.BlogPost
     template_name = 'blog/blog_post_page.html'
+
+
+class TaggedPostView(ListView):
+
+    model = models.BlogPost
+    template_name = "home/home.html"
+    paginate_by = 10
+    paginate_orphans = 3
+
+    def dispatch(self, *args, **kwargs):
+        self.tag = get_object_or_404(models.Tag, slug=self.kwargs.get('slug'))
+        return super(TaggedPostView, self).dispatch(*args, **kwargs)
+
+    def get_queryset(self):
+        return models.BlogPost.objects.get_published_posts_for_site(
+            self.request.site
+        ).filter(tags__id=self.tag.id).order_by('-publish_date')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TaggedPostView, self).get_context_data(*args, **kwargs)
+        context['tag'] = self.tag
+        return context
